1689161c5be176b3b1410323f304a6c73d2d11ef
EmmyRecent <chukwuemekaeziagulu@gmail.com>
2024-05-31 20:09:56 +0100

    directory fix

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 import express from "express";
 import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const port = process.env.PORT || 3000;
 const date = new Date();
 const year = date.getFullYear();
 
+// For ES Modules, you need to handle __dirname differently
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // Serve static files
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Set the views directory and view engine
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+// console.log("Views directory:", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render("pages/home.ejs", { currentYear: year });
